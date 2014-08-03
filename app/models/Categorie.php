3a8b0bb6c3e771d8f41dfc8fbb3f23d52296cb6c
Marc Ducobu <marc@champs-libres.coop>
2014-08-03 14:14:43 +0200

    descendantCategoriesArray & descendantCategoriesIdArray fct

--- app/models/Categorie.php
+++ app/models/Categorie.php
@@ -19,4 +19,32 @@ class Categorie extends Eloquent {
 	public function categories() {
 		return Categorie::where('parent_id', $this->id)->get();
 	}
+
+	/**
+	 * All the descendant categories ( recursive ) in an Array
+	 */
+	public function descendantCategoriesArray() {
+		$subCategories = $this->categories();
+
+		$ret = array();
+		array_push($ret, $this);
+
+		foreach ($subCategories as $c) {	
+			$ret = array_merge($ret, $c->descendantCategoriesArray());
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * All the descendant categories id ( recursive ) in an Array
+	 */
+	public function descendantCategoriesIdArray() {
+		function getId($c) 
+		{
+			return $c->id;
+		}
+
+		return array_map("getId", $this->descendantCategoriesArray());
+	}
 }
